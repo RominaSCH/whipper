08fceb6af6d62941f3b7fe560a1de66b06efb790
Romina Schann <cuqi213@gmail.com>
2021-05-17 12:08:58 +0900

    signin email/google/github and log out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,21 +1,36 @@
 import React, { useState } from "react";
-import {HashRouter as Router, Route, Switch} from "react-router-dom";
-import Auth from "../routes/Ayth";
+import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import Authen from "../routes/Authen";
 import Home from "../routes/Home";
+import Profile from "../routes/Profile";
+import Navigation from "../components/Navigation";
 
-const AppRouter = ({isLoggedIn}) => {
-    return (
-        <Router>
-            <Switch>
-                {isLoggedIn ? (<>
-                <Route exact path="/">
-                    <Home />
-                </Route> 
-                </>)
-                    : <Route exact path="/"><Auth /></Route>}
-            </Switch>
-        </Router>
-    );
+const AppRouter = ({ isLoggedIn }) => {
+  return (
+    <Router>
+      {isLoggedIn && <Navigation />}
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            <Redirect from="*" to="/" />
+          </>
+        ) : (
+          <>
+            <Route exact path="/">
+              <Authen />
+            </Route>
+            <Redirect from="*" to="/" />
+          </>
+        )}
+      </Switch>
+    </Router>
+  );
 };
 
 export default AppRouter;
---
+++ src/routes/Authen.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { authService, firebaseInstance } from "../fireb";
+
+const Authen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault(); //기본 행위가 실행되는 것을 막는 함수
+    //새로고침하면 자료 다 날아가니까 막아야하므로 썼음
+
+    try {
+      let data;
+      if (newAccount) {
+        //create account
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        //log in
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  const toggleAccount = () => {
+    setNewAccount((prev) => !prev);
+  };
+
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <inputs
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+        {error}
+      </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign in" : "Create Account"}
+      </span>
+      <div>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Authen;
