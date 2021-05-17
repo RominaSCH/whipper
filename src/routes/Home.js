72773970e5483623a5992a951d8b718ddf189e1c
Romina Schann <cuqi213@gmail.com>
2021-05-17 12:16:17 +0900

    database start

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [whip, setWhip] = useState("");
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+    const onChange = (e) => {
+        const {target : {value}} = e;
+        setWhip(e);
+    };
+    return (
+        <div>
+            <form>
+                <input value={whip} onChange={onChange} type="textarea" placeholder="What's on your mind?" maxLength={120} />
+                <input type="submit" placeholder="whip"
+            </form>
+        </div>
+    );
+};
 export default Home;
